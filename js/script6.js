966578e92c1854c42a8858729a09c2fc475bbfdb
Ayduxa2 <ablaev.a.i.1.18@gmail.com>
2018-11-20 21:27:44 +0300

    file added

---
+++ js/script6.js
@@ -0,0 +1,22 @@
+var num1 = +prompt("enter num1","")
+var sign = prompt("enter sign","")
+var num2 = +prompt("enter num2",""), result
+if (sign == "+" ){
+result= num1+ num2
+alert(result);}
+else if (sign == "-" ){
+result = num1 - num2
+alert(result)
+}
+else if (sign == "*" ){
+  result = num1*num2
+  alert(result)
+}
+  else if (sign = "/" )
+  if (num2>0){
+    result=num1/num2
+  }
+else {
+  if (num2 == "0")
+  alert("u are dibil? na 0 delit nizya")
+}
