18c656bbd352a04079743792710270b898d9d827
csl4ik <gipolllll@gmail.com>
2018-11-21 11:31:50 +0300

    file added

--- js/script6.js
+++ js/script6.js
@@ -1,3 +1,50 @@
+
+/*var AA = prompt("В каком году ты поступил в КИПУ?","")
+if (AA != 2018){
+  alert("False!!!")
+  alert("think about it")}
+else
+alert("hello pervash")*/
+/*var A= prompt("A=","")
+var B= prompt("B=","")
+if (A>B)
+alert(C=A)
+else
+alert("C равно", C=B)*/
+/*var X = prompt("X=","")
+var Y = prompt("Y=","")
+if (X>Y){
+(C=X)
+alert(X=Y)
+alert(Y=C)}
+else{
+alert(X)
+alert(Y)}*/
+/*var A = prompt("fsdf","")
+if(0) then
+   console.log("hello")*/
+/*var A = prompt("A","")
+var B = prompt("B","")
+var C = prompt("C",""),D
+if (A>B)
+D=A
+else
+D=B
+if (C>D)
+D=C
+else then
+console.log(D);*/
+//5,6
+/*var MAMA= prompt("MAMA age=","")
+var DAD= prompt("DAD age=","")
+if (MAMA>DAD) {
+console.log("мама старше")}
+else
+if (MAMA<DAD)
+{console.log("папа старше")}
+else {
+console.log("родители ровесники");}*/
+//7
 var num1 = +prompt("enter num1","")
 var sign = prompt("enter sign","")
 var num2 = +prompt("enter num2",""), result
